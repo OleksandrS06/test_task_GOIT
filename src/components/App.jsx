762ae469cc15056f7596336d4961ff4a7d751409
OleksandrS06 <112249299+OleksandrS06@users.noreply.github.com>
2022-12-27 00:45:26 +0100

    for check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Card from './Card';
+
+export class App extends Component {
+  state = {
+    followers: 100500,
+    isFollowing: false,
+    condition: 'Follow',
+  };
+
+  componentDidMount() {
+    const followers = JSON.parse(localStorage.getItem('follower'));
+    if (followers) {
+      this.setState({
+        followers: followers.followers,
+        isFollowing: followers.isFollowing,
+      });
+    }
+  }
+  componentDidUpdate(p, prevState) {
+    if (prevState !== this.state) {
+      localStorage.setItem('follower', JSON.stringify(this.state));
+    }
+  }
+  followBtnHandler = e => {
+    const { isFollowing } = this.state;
+    if (isFollowing === false) {
+      this.setState(prevState => {
+        return {
+          followers: prevState.followers + 1,
+          isFollowing: true,
+          condition: 'following',
+        };
+      });
+    } else {
+      this.setState(prevState => {
+        return {
+          followers: prevState.followers - 1,
+          isFollowing: false,
+          condition: 'Follow',
+        };
+      });
+    }
+  };
+
+  render() {
+    return (
+      <Card
+        followers={this.state.followers}
+        condition={this.state.condition}
+        followBtnHandler={this.followBtnHandler}
+        isFollowing={this.state.isFollowing}
+      />
+    );
+  }
+}
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,47 @@
+import css from './Сard.module.css';
+
+import Logo from '../../images/Logo.svg';
+import Hansel from '../../images/Hansel.png';
+
+const Card = ({ followers, condition, isFollowing, followBtnHandler }) => {
+  const followersToDisplay =
+    String(followers).slice(0, 3) + ',' + String(followers).slice(3);
+  return (
+    <div className={css.wrap}>
+      <div className={css.logoContainer}>
+        <img
+          src={Logo}
+          alt="GO_IT logo"
+          srcSet={Logo}
+          width="88"
+          className={css.logo}
+        />
+      </div>
+
+      <div className={css.userInfo}>
+        <div className={css.line}></div>
+        <div className={css.avatarWrap}>
+          <img
+            src={Hansel}
+            alt="boy-picture"
+            srcSet={Hansel}
+            width="72"
+            className={css.avatar}
+          />
+        </div>
+        <div className={css.userStat}>
+          <p className={css.userStat__text}>777 tweets</p>
+          <p className={css.userStat__text}>{followersToDisplay} Followers</p>
+          <button
+            onClick={followBtnHandler}
+            className={isFollowing ? css.isFollowing : css.btn}
+          >
+            {condition}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
